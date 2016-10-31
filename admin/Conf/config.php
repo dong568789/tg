6f6c980faa33c0a1f3ed00338c167f0751f6a1db
chen <chen_yw@126.com>
2016-10-31 16:19:05 +0800

    * modify the ta's database config

--- admin/Conf/config.php
+++ admin/Conf/config.php
@@ -12,10 +12,10 @@ return array(
 	'DEFAULT_CHARSET' => 'UTF-8', 
 	// 数据库配置,模块的配置在模块对于的model里面设置
 	'DB_TYPE' => 'mysql',
-        'DB_HOST' => '10.13.58.56',
+        'DB_HOST' => '192.168.1.32',
         'DB_NAME' => 'db_youxia_new',    
-	'DB_USER' => 'User_youxia_tg',   
-	'DB_PWD' => 'YOUxiadb@2016', 
+	'DB_USER' => 'root',   
+	'DB_PWD' => 'hy123456', 
 	'URL_PATHINFO_DEPR'=>'/',
 	//配置后缀
 	'TMPL_TEMPLATE_SUFFIX' => '.tpl',
