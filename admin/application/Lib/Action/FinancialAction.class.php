50d16f4bf9ee2ae62563e9dcf46171f448c510cf
张华勇 <282477161@qq.com>
2017-08-11 15:20:31 +0800

    *财务收支

--- admin/application/Lib/Action/FinancialAction.class.php
+++ admin/application/Lib/Action/FinancialAction.class.php
@@ -23,22 +23,22 @@ class FinancialAction extends CommonAction
 
         echo json_encode(array(
             'rows' => $financial,
-            'total' =>count($financial)
+            'total' => count($financial)
         ));
     }
 
     public function export()
     {
         $financial = $this->getData();
-        if(empty($financial)){
+        if (empty($financial)) {
             $this->error('数据获取失败，没有符合条件的数据');
         }
         $item = array();
-        foreach($financial as $key=>$value){
+        foreach ($financial as $key => $value) {
             $item[$key] = $value['time'];
         }
 
-        array_multisort($item,SORT_ASC,$financial);
+        array_multisort($item, SORT_ASC, $financial);
         $title['time'] = '日期';
         $title['amount'] = '游戏直充';
         $title['buy_coin'] = '买币直充';
@@ -54,10 +54,10 @@ class FinancialAction extends CommonAction
         $title['expend'] = '支出';
         $title['expend_qz'] = '潜在支出';
         $title['earning_qz'] = '预估收入';
-       
+
         $this->exportFile($title, $financial);
     }
-    
+
     public function getData()
     {
         $financialModel = M('static_financial');
@@ -69,38 +69,41 @@ class FinancialAction extends CommonAction
             ->order($sort)
             ->select();
 
-        $sumEarning = $sumExpend = $sumExpend_qz = $smEarning_qz = 0;
-        foreach($financial as &$value){
+        $sumEarning = $sumExpend = $sumExpend_qz = $smEarning_qz = $sumAgentCoin = $sumGameCoin = $sumVoucher = 0;
+        foreach ($financial as &$value) {
             $value['earning'] = $value['amount'] + $value['buy_coin'] + $value['app'] + $value['buy_voucher'] +
                 $value['offline_coin'];
             $value['expend'] = $value['cash_over'] + $value['cp_into'];
-            $value['agent_coin'] =  intval($value['agent_coin']/10);
-            $value['game_coin'] =  intval($value['game_coin']/10);
+            $value['agent_coin'] = intval($value['agent_coin'] / 10);
+            $value['game_coin'] = intval($value['game_coin'] / 10);
 
             $value['expend_qz'] = $value['agent_coin'] + $value['game_coin'] +
-                $value['voucher']/2;
-            $value['earning_qz'] = $value['earning'] - $value['expend']-$value['expend_qz'];
+                $value['voucher'] / 2;
+            $value['earning_qz'] = $value['earning'] - $value['expend'] - $value['expend_qz'];
 
             $sumEarning += $value['earning'];
             $sumExpend += $value['expend'];
             $sumExpend_qz += $value['expend_qz'];
             $smEarning_qz += $value['earning_qz'];
+            $sumAgentCoin += $value['agent_coin'];
+            $sumGameCoin += $value['game_coin'];
+            $sumVoucher += $value['voucher'];
 
 
         }
 
-        array_unshift($financial,array(
+        array_unshift($financial, array(
             'time' => '统计',
-            'amount'=>'-',
-            'buy_coin'=>'-',
-            'app'=>'-',
-            'cash_over'=>'-',
-            'buy_voucher'=>'-',
-            'offline_coin'=>'-',
-            'cp_into'=>'-',
-            'agent_coin'=>'-',
-            'game_coin'=>'-',
-            'voucher'=>'-',
+            'amount' => '-',
+            'buy_coin' => '-',
+            'app' => '-',
+            'cash_over' => '-',
+            'buy_voucher' => '-',
+            'offline_coin' => '-',
+            'cp_into' => '-',
+            'agent_coin' => $sumAgentCoin,
+            'game_coin' => $sumGameCoin,
+            'voucher' => $sumVoucher,
             'earning' => $sumEarning,
             'expend' => $sumExpend,
             'expend_qz' => $sumExpend_qz,
@@ -115,8 +118,8 @@ class FinancialAction extends CommonAction
         $id = isset($_POST["id"]) ? (int)$_POST["id"] : '';
         $money = isset($_POST["offline_coin"]) ? $_POST["offline_coin"] : 0;
 
-        if(empty($id)){
-            $this->ajaxReturn('参数错误','error',0);
+        if (empty($id)) {
+            $this->ajaxReturn('参数错误', 'error', 0);
         }
 
         $modal = M('static_financial');
@@ -127,14 +130,14 @@ class FinancialAction extends CommonAction
         $data["offline_coin"] = $money;
         $result = $modal->where($condition)->save($data);
 
-        $this->insertLog($_SESSION['adminname'],'修改买币金额', 'FinancialAction.class.php', 'editOfflineCoin',
+        $this->insertLog($_SESSION['adminname'], '修改买币金额', 'FinancialAction.class.php', 'editOfflineCoin',
             $data['updatetime'],
-            $_SESSION['adminname']."重置【".$financial['time']."】买币金额,时间【".$financial["createtime"]."】,原始金额：" .$financial["cash_over"].",修改金额:".$money);
-        if($result){
-            $this->ajaxReturn($result,'success',1);
+            $_SESSION['adminname'] . "重置【" . $financial['time'] . "】买币金额,时间【" . $financial["createtime"] . "】,原始金额：" . $financial["cash_over"] . ",修改金额:" . $money);
+        if ($result) {
+            $this->ajaxReturn($result, 'success', 1);
             exit();
-        }else{
-            $this->ajaxReturn('未能更新成功','fail',0);
+        } else {
+            $this->ajaxReturn('未能更新成功', 'fail', 0);
             exit();
         }
     }
@@ -144,22 +147,22 @@ class FinancialAction extends CommonAction
         $startTime = I('request.startdate');
         $endTime = I('request.enddate');
         $where = array();
-        if(empty($startTime) || empty($endTime)){
-            $startTime =date('Y-m-01');
+        if (empty($startTime) || empty($endTime)) {
+            $startTime = date('Y-m-01');
             $endTime = date('Y-m-d');
         }
 
-        if(strtotime($endTime)  >= mktime(0,0,0)){
-            $endTime = date('Y-m-d', strtotime('-1 day', mktime(0,0,0)));
+        if (strtotime($endTime) >= mktime(0, 0, 0)) {
+            $endTime = date('Y-m-d', strtotime('-1 day', mktime(0, 0, 0)));
         }
 
-        if($startTime > $endTime){
+        if ($startTime > $endTime) {
             $itemTime = $startTime;
             $startTime = $endTime;
             $endTime = $itemTime;
         }
 
-       $where['time'] = array(array('EGT', $startTime), array('ELT', $endTime));
+        $where['time'] = array(array('EGT', $startTime), array('ELT', $endTime));
 
         return $where;
     }
@@ -176,20 +179,20 @@ class FinancialAction extends CommonAction
      * @param string $outputFileName 要保存的文件名
      * @param string $sheetTitle 工作表名称
      */
-    public function exportFile(array $title, array $data,$outputFileName='')
+    public function exportFile(array $title, array $data, $outputFileName = '')
     {
         ob_clean();
-        if (empty($outputFileName)){
-            $outputFileName = date('Y-m-d').'-'.md5(time()).'.xls';
+        if (empty($outputFileName)) {
+            $outputFileName = date('Y-m-d') . '-' . md5(time()) . '.xls';
         }
         include_once dirname(__FILE__) . "/../../ORG/PHPExcel.php";
         include_once dirname(__FILE__) . "/../../ORG/PHPExcel/IOFactory.php";
         $objExcel = new PHPExcel();
         $path = pathinfo($outputFileName);
-        if($path['extension'] == 'xlsx'){
+        if ($path['extension'] == 'xlsx') {
             Vendor("PHPExcel.PHPExcel.Writer.Excel2007");
             $objWriter = new PHPExcel_Writer_Excel2007($objExcel);
-        }else{
+        } else {
             Vendor("PHPExcel.PHPExcel.Writer.Excel2007");
             $objWriter = new PHPExcel_Writer_Excel2007($objExcel);
         }
@@ -202,19 +205,16 @@ class FinancialAction extends CommonAction
         $objActSheet->setTitle('sheet0');
         //为第一行赋值
         $i = 0;
-        foreach ($title as $key=>$value)
-        {
-            $index = 65+$i;
-            $objActSheet->setCellValue(chr($index).'1', $value);
+        foreach ($title as $key => $value) {
+            $index = 65 + $i;
+            $objActSheet->setCellValue(chr($index) . '1', $value);
             $i++;
         }
         $arrKeys = array_keys($title);
         $data = array_values($data);
-        foreach ($data as $key=>$value)
-        {
-            for ($m=0;$m<=$i;$m++)
-            {
-                $objActSheet->setCellValueExplicit(chr($m+65).($key+2),$value[$arrKeys[$m]],\PHPExcel_Cell_DataType::TYPE_STRING);
+        foreach ($data as $key => $value) {
+            for ($m = 0; $m <= $i; $m++) {
+                $objActSheet->setCellValueExplicit(chr($m + 65) . ($key + 2), $value[$arrKeys[$m]], \PHPExcel_Cell_DataType::TYPE_STRING);
             }
         }
         $today = date('Y/m/d/');
@@ -234,30 +234,34 @@ class FinancialAction extends CommonAction
     }
 }
 
-class Dir{
+class Dir
+{
 
     //判断目录是否存在
-    static public function is_dir($dir){
+    static public function is_dir($dir)
+    {
         if (is_dir($dir)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     //递归创建目录
-    static public function mkdir_digui($dir, $mode = 0777){
-        if (is_dir($dir) || @mkdir($dir,$mode)) {
+    static public function mkdir_digui($dir, $mode = 0777)
+    {
+        if (is_dir($dir) || @mkdir($dir, $mode)) {
             return true;
         }
-        if (!self::mkdir_digui(dirname($dir),$mode)) {
+        if (!self::mkdir_digui(dirname($dir), $mode)) {
             return false;
         }
-        return @mkdir($dir,$mode);
+        return @mkdir($dir, $mode);
     }
 
     //自动创建目录
-    static public function create_dir_auto ($dir) {
+    static public function create_dir_auto($dir)
+    {
         //判断有没有主文件夹，如果没有则创建
         if (!self::is_dir($dir)) {
             //递归创建目录
